665de1b0f4932b12fd303db2f99e0dc9496e8562
Denilson Gustavo <denilsongustavoln@gmail.com>
2024-01-30 17:01:24 -0300

    inserindo css

--- my_site/src/App.js
+++ my_site/src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import HelloWorld from "./components/HelloWorld";
+import Frase from "./components/Frase"
+import SayMyName from "./components/SayMyName";
+import Pessoa from "./components/Pessoa";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld/>
+      <SayMyName nome="Mateus"/>
+      <SayMyName nome="João"/>
+      <Pessoa nome="Denilson" idade="24" profissao="Estudante" foto="https://via.placeholder.com/150"/>
     </div>
+
   );
 }
 
---
+++ my_site/src/components/SayMyName.js
@@ -0,0 +1,8 @@
+function SayMyName(props){
+    return(
+        <div>
+            <p>Fala aí {props.nome}, tranquilo? </p>
+        </div>
+    )
+}
+export default SayMyName
